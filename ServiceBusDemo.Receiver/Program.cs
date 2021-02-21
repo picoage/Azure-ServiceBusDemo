8b01660fff983151b31f59c8d39ff3a558f209e4
Vishnu Kumar <microsophtdeveloper@outlook.com>
2021-02-21 22:11:29 +0000

    Add project files.

---
+++ ServiceBusDemo.Receiver/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ServiceBusDemo.Receiver
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "";
+        const string QueueName = "";
+        static IQueueClient queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+        static void Main(string[] args)
+        {
+       
+          ReceiveMessageAsync().GetAwaiter().GetResult(); 
+            Console.WriteLine("Completed!");          
+        }
+
+        public static async Task ReceiveMessageAsync()
+        {
+         
+            RegisterMessageHandler();
+            Console.ReadLine(); 
+            //Thread.Sleep(10000);
+            await queueClient.CloseAsync();
+        }   
+
+        private static void RegisterMessageHandler()
+        {
+            var messageHandlerOption = new MessageHandlerOptions(ExceptionReceivedHandler) { MaxConcurrentCalls = 1, AutoComplete = false };
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOption); 
+        }
+
+        private static async Task ProcessMessagesAsync(Message message, CancellationToken cancellationToken)
+        {
+            if (message.Body != null)
+                Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} body:{Encoding.UTF8.GetString(message.Body)}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine($"An Error {arg.Exception}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ServiceBusDemo.Sender/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+string connectionString = "";
+string QueueName = "";
+SendMessage().GetAwaiter().GetResult();
+
+
+async Task SendMessage()
+{
+    var queueClient = new QueueClient(connectionString, QueueName);
+
+
+
+    for (int i = 0; i < 10; i++)
+    {
+        var content = $"Message{i}";
+
+        var message = new Message(Encoding.UTF8.GetBytes(content));
+
+       await queueClient.SendAsync(message);     
+
+    }
+  Console.WriteLine("MessageSent");
+   await queueClient.CloseAsync();
+}
+
+
+
+
+
